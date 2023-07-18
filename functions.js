14bd89043b1cf68e665423cf9c18cdfedac3d687
W S Souza <dev9.ws@gmail.com>
2023-07-18 15:55:35 -0300

    gerarGráfico

--- functions.js
+++ functions.js
@@ -145,3 +145,35 @@ getPedidoLI(a,b)
     console.error(error);
   });
 
+  function gerarGrafico() {
+    const dados = getPedidoLI();
+  
+    const labels = dados.map(item => item.label);
+    const valores = dados.map(item => item.valor);
+  
+    const ctx = document.getElementById('meuGrafico').getContext('2d');
+    const meuGrafico = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Vendas por Produto',
+            data: valores,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+  
+  module.exports = {gerarGrafico, getPedidoLI}
--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express');
-const app = express()
+const app = express();
 const porta = 3000;
+const func = require('./functions');
 
-const methodOverride = require('method-override');
-
-const axios = require('axios');
-const apiUrl = 'https://api.awsli.com.br/v1/produto';
-const chaveApi = 'sua_chave_api';
-const chaveAplicacao = 'sua_chave_aplicacao';
-
-
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
-    console.log(`Servidor Conectado na porta: ${porta}`);
-    res.send(`Servidor Conectado na porta: ${porta}`)
-    res.render(index)
-})
+app.get('/', async (req, res) => {
+  console.log(`Servidor Conectado na porta: ${porta}`);
+  try {
+    const dadosGrafico = await func.getPedidoLI();
+    res.render('index', { dadosGrafico });
+  } catch (error) {
+    console.error(error);
+    res.send('Erro ao gerar gráfico');
+  }
+});
 
-app.listen(porta, ()=>{
-    console.log(`Conexão estabelecida na porta: ${porta}`);
-})
+app.listen(porta, () => {
+  console.log(`Conexão estabelecida na porta: ${porta}`);
+});
